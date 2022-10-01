36f491931feece59485e929cf5d4500b8e0a4d20
Gabriel Sena <stardusteight.d4cc@gmail.com>
2022-10-01 14:34:36 -0300

    finalizando projeto

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -12,7 +12,6 @@ import { getSearchPageVideos } from '../store/reducers/getSearchPageVideos'
 import { mockDataSearchTerm } from '../../mockData'
 import { motion } from 'framer-motion'
 
-
 export default function Search() {
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
@@ -31,18 +30,6 @@ export default function Search() {
 
   return (
     <>
-      <head>
-        <meta charSet="UTF-8" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
-          rel="stylesheet"
-        />
-        <title>{searchTerm} in Youtube</title>
-      </head>
       <div className="h-screen overflow-hidden">
         <div className="h-[5.4vh] md:h-[7.5vh]">
           <Navbar />
